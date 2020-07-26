5b619973c62e3f8a1a544f4a03241a68cc639f84
Tony Kim <hjkmines@gmail.com>
2020-07-26 11:05:22 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const app = express()
+
+app.use(bodyParser.json())
+
+app.post("/messages", (request, response) => {
+  const message = `The message you sent me was: ${request.body.message}`
+
+  response.json({message})
+})
+
+app.listen(process.env.PORT || 9000, () => {
+  console.log("Listening...")
+})
